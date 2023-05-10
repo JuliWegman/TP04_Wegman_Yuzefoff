f0f3e1dec56a5f879bce0e1b781f1fdf88eba9e6
JULIÁN WEGMAN <47806018@comunidadort.edu.ar>
2023-05-10 09:06:14 -0300

    cuarto

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,14 +6,13 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        
         ViewBag.Paquetes=OrtWorld.Paquetes;
         return View();
     }
     public IActionResult SelectPaquete(){
         ViewBag.hoteles=OrtWorld.ListaHoteles;
         ViewBag.aereos=OrtWorld.ListaAereos;
-        ViewBag.Excursiones=OrtWorld.ListaExcursiones;
+        ViewBag.excursiones=OrtWorld.ListaExcursiones;
         ViewBag.destinos=OrtWorld.ListaDestinos;
         return View();
     }
--- Models/OrtWorld.cs
+++ Models/OrtWorld.cs
@@ -1,8 +1,8 @@
 class OrtWorld{
 public static List<string> ListaDestinos {get; private set;}=new List<string>{"Buenos Aires", "Bariloche", "Mar del plata", "Viedma", "San Rafael", "Rawson","Puerto Madrin", "Neuquén","San Martín de Tucuman","Macabi san Miguel"};
 public static List<string> ListaHoteles {get; private set;}=new List<string>{"Hilton.jfif", "Llao llao.jpg", "Costa Galana.webp", "Casa Crespo Hotel Boutique.jfif", "Patios del Mediterraneo Apart Hotel.jfif", "Libertador.jfif","Rayentray Puerto Madryn.jfif", "La Morada Petit Hotel.jfif","Hotel Bicentenario Suites & Spa.jfif","macabi.jpg"};
-public static List<string> ListaAereos {get; private set;}=new List<string>{"Buenos Aires", "Bariloche", "Mar del plata", "Viedma", "San Rafael", "Rawson","Puerto Madrin", "Neuquén","San Martín de Tucuman","Macabbi san Miguel"};
-public static List<string> ListaExcursiones {get; private set;}=new List<string>{"Buenos Aires", "Bariloche", "Mar del plata", "Viedma", "San Rafael", "Rawson","Puerto Madrin", "Neuquén","San Martín de Tucuman","Macabbi san Miguel"};
+public static List<string> ListaAereos {get; private set;}=new List<string>{"aerolineas_argentinas.png", "Alaska Airlines.png", "British Airways.png", "Delta.png", "Ethiopian.png", "Japan Airlines.png","LAN.jfif", "Lufthansa.png","Singapur.png","turkish_airlines.png"};
+public static List<string> ListaExcursiones {get; private set;}=new List<string>{"lago.jpg", "playa.jpg", "montaña.jpg", "glaciar.jfif", "obelisko.jfif", "cataratas.jfif","cierro.jpg", "ciudad.jpg","cotur.jfif","turismo.jpg"};
 public static Dictionary<string, Paquete> Paquetes {get; private set;}
 public static bool IngresarPaquete(string destino, Paquete paquete)
 {
