8e782527ed2e9fa621c50043b834a0cb358ffecc
JULIÁN WEGMAN <47806018@comunidadort.edu.ar>
2023-05-15 10:40:55 -0300

    sexto commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,11 +9,25 @@ public class HomeController : Controller
         ViewBag.Paquetes=OrtWorld.Paquetes;
         return View();
     }
-    public IActionResult SelectPaquete(){
+    public IActionResult SelectPaquete()
+    {
         ViewBag.hoteles=OrtWorld.ListaHoteles;
         ViewBag.aereos=OrtWorld.ListaAereos;
         ViewBag.excursiones=OrtWorld.ListaExcursiones;
         ViewBag.destinos=OrtWorld.ListaDestinos;
         return View();
+    }
+    public IActionResult GuardarPaquete(string destinoSeleccionado, int HotelElegido, int AereoElegido, int ExcursionElegido)
+    {
+         Paquete x = new Paquete(OrtWorld.ListaHoteles[HotelElegido],OrtWorld.ListaAereos[AereoElegido],OrtWorld.ListaExcursiones[ExcursionElegido]);
+         bool Existe=OrtWorld.IngresarPaquete(destinoSeleccionado,x);
+        if(Existe){
+        OrtWorld.Paquetes.Add(destinoSeleccionado,x);
+        return RedirectToAction("Index");
+        }else{
+            return RedirectToAction("SelectPaquete");
+        }
+        
+        
     }
 }
