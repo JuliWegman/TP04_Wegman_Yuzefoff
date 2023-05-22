4de8bdcc74b99d7afb3101666cb5c2d2f771f08f
FACUNDO MANUEL YUZEFOFF <47344479@ort>
2023-05-22 10:45:44 -0300

    imagenes

--- Models/OrtWorld.cs
+++ Models/OrtWorld.cs
@@ -1,7 +1,7 @@
 class OrtWorld{
 public static List<string> ListaDestinos {get; private set;}=new List<string>{"Buenos Aires", "Bariloche", "Mar del plata", "Viedma", "San Rafael", "Rawson","Puerto Madrin", "Neuquén","San Martín de Tucuman","Macabi san Miguel"};
-public static List<string> ListaHoteles {get; private set;}=new List<string>{"Hilton.jfif", "HotelLlao.jpg", "Costa Galana.webp", "Casa Crespo Hotel Boutique.jfif", "Patios del Mediterraneo Apart Hotel.jfif", "Libertador.jfif","Rayentray Puerto Madryn.jfif", "La Morada Petit Hotel.jfif","Hotel Bicentenario Suites & Spa.jfif","macabi.jpg"};
-public static List<string> ListaAereos {get; private set;}=new List<string>{"aerolineas_argentinas.png", "Alaska Airlines.png", "British Airways.png", "Delta.png", "Ethiopian.png", "Japan Airlines.png","LAN.jfif", "Lufthansa.png","Singapur.png","turkish_airlines.png"};
+public static List<string> ListaHoteles {get; private set;}=new List<string>{"Hilton.jfif", "Hotel llao llao.jpg", "Costa Galana.webp", "Casa Crespo Hotel Boutique.jfif", "Patios del Mediterraneo Apart Hotel.jfif", "Libertador.jfif","Rayentray Puerto Madryn.jfif", "La Morada Petit Hotel.jfif","Hotel Bicentenario Suites & Spa.jfif","macabi.jpg"};
+public static List<string> ListaAereos {get; private set;}=new List<string>{"aerolineas_argentinas.png", "Alaska Airlines.png", "British Airways.png", "Delta.png", "Ethiopian.png", "Japan Airlines.png","LAN.png", "Lufthansa.png","Singapur.png","turkish_airlines.png"};
 public static List<string> ListaExcursiones {get; private set;}=new List<string>{"lago.jpg", "playa.jpg", "montaña.jpg", "glaciar.jfif", "obelisko.jfif", "cataratas.jfif","cierro.jpg", "ciudad.jpg","cotur.jfif","turismo.jpg"};
 public static Dictionary<string, Paquete> Paquetes {get; private set;}=new Dictionary<string, Paquete>();
 
